e063c5621d3d33ff11250df50c7ef40b25ff786a
PigOneDragon <18137410276@189.cn>
2019-07-18 17:36:03 +0800

    点赞功能实现，暂时没有用户限制一次功能

--- WeChatPro/miniprogram/pages/bxq/bxq.js
+++ WeChatPro/miniprogram/pages/bxq/bxq.js
@@ -5,9 +5,62 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+   /*  // 切换图片的类
+    showView: false, */
+    // 给点赞的元素添加ID，匹配哪个内容发生了点击
+    imgTxtList: [{
+      id:'001',
+      showView: true,
+      desc: '公平保险就是保险期望损失为0的保单，即保单的期望损失等于保险费，是基于概率理论、期望理论引申而来的。',
+      imgUrls: ['../../images/b_1.jpg', '../../images/b_1.jpg', '../../images/b_1.jpg'],
+      view: 105,
+      ly: 32,
+      good: 10
+    }, {
+      id: '002',
+      showView: true,
+      desc: '公平保险就是保险期望损失为0的保单，即保单的期望损失等于保险费，是基于概率理论、期望理论引申而来的。',
+      imgUrls: ['../../images/b_1.jpg', '../../images/b_1.jpg', '../../images/b_1.jpg'],
+      view: 105,
+      ly: 32,
+      good: 12
+    }, {
+      id: '003',
+      showView: true,
+      desc: '公平保险就是保险期望损失为0的保单，即保单的期望损失等于保险费，是基于概率理论、期望理论引申而来的。',
+      imgUrls: ['../../images/b_1.jpg', '../../images/b_1.jpg', '../../images/b_1.jpg'],
+      view: 105,
+      ly: 32,
+      good: 13
+    }, {
+      id: '004',
+      showView: true,
+      desc: '公平保险就是保险期望损失为0的保单，即保单的期望损失等于保险费，是基于概率理论、期望理论引申而来的。',
+      imgUrls: ['../../images/b_1.jpg', '../../images/b_1.jpg', '../../images/b_1.jpg'],
+      view: 105,
+      ly: 32,
+      good: 14
+    }]
   },
-
+  // 给点赞功能绑定点击事件，点击时切换图片
+  // （一个用户点赞一次？后期更新）
+  handleClick: function (e) {
+    /*  that.setData({
+       showView: (!that.data.showView)
+     }); */
+    //  点击实现点赞数加1
+    var info_id = e.currentTarget.dataset.id; //返回触发事件的ID
+    console.log(info_id);
+    for (var i = 0; i < this.data.imgTxtList.length; i++) {
+      if (this.data.imgTxtList[i].id == info_id) {
+        this.setData({
+            // 改变对象 i 节点的图片显示的值为true,当前的点赞数加一
+            ["imgTxtList[" + i + "].showView"]: false,
+            ["imgTxtList[" + i + "].good"]: parseInt(this.data.imgTxtList[i].good) + 1
+          });
+      }
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
